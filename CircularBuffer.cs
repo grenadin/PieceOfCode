ceb03121d0eaa8f8a78b712b8e90f011c0dfa07e
grenadin <harirak@hotmail.co.th>
2020-06-14 23:29:40 +0700

    Add project files.

---
+++ CircularBuffer.cs
@@ -0,0 +1,48 @@
+﻿namespace PieceOfCode
+{
+    class CircularBuffer
+    {
+        double[] _buffer;
+        int _start;
+        int _end;
+        int capacity;
+
+        public int Capacity { get => _buffer.Length;set => capacity = value;  }
+        public bool IsEmpty { get => _end == _start; }
+        public bool IsFull { get => (_end + 1) % _buffer.Length == _start; }
+        public CircularBuffer() : this(10)
+        {
+            System.Console.WriteLine("hi");
+        }
+        public CircularBuffer(int capacity)
+        {
+            _buffer = new double[capacity + 1];
+            _start = 0;
+            _end = 0;
+        }
+
+        
+
+        public void Write(double value)
+        {
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+            if (_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+
+        }
+
+        public double Read()
+        {
+            var result = _buffer[_start];
+            _start = (_start + 1) % _buffer.Length;
+            return result;
+        }
+    }
+
+   
+
+       
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+
+namespace PieceOfCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            var buffer = new CircularBuffer(3);
+
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while(!buffer.IsEmpty) //do it when true
+            {
+                Console.WriteLine(buffer.Read()); 
+            }
+            //Console.WriteLine(sum);
+        }
+
+      
+    }
+
+   
+
+       
+}
